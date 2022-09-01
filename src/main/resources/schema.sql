dccd22aa2d1afeab40a834f2be475739155cd7fa
Dharwin R V J <dharwinruppa1422@gmail.com>
2022-09-01 12:11:16 +0530

    sca

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,7 +3,7 @@ create database if not exists servletcrud;
 use servletcrud;
 -- create table for users
 create table if not exists users (
-    id int(3) primary key auto_increment,
+    id int(3) serial primary key,
     name varchar(30) not null,
     email varchar(30) not null,
     country varchar(30) not null
--- target/servlet-crud-api/index.jsp
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-    <title>User CRUD</title>
-</head>
-<body>
-<a href="/users">click here</a>
-</body>
-</html>
