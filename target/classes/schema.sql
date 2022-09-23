e1b983c623bfc22d6a904db74b5168e86e6b23dd
Dharwin R V J <dharwinruppa1422@gmail.com>
2022-09-23 14:57:09 +0530

    sca

--- target/classes/schema.sql
+++ target/classes/schema.sql
@@ -3,7 +3,7 @@ create database if not exists servletcrud;
 use servletcrud;
 -- create table for users
 create table if not exists users (
-    id int(3) serial primary key,
+    id int(3) auto_increment primary key,
     name varchar(30) not null,
     email varchar(30) not null,
     country varchar(30) not null
